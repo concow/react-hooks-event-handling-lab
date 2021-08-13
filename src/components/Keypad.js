30cd825ec1f5c3eb087e9b1cee7e65cee76036b1
Connor Cowan <ccowan003@comcast.net>
2021-08-13 11:13:28 -0700

    commit tree e99be0074f7d1ec670fb9bdcd6b8083b905a9cb5

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+//import React from 'react'
+
+export default function KeyPad() {
+    
+    function handleChange() {
+        console.log("Entering password...")
+    }
+
+    return (
+        //render an input with the right type
+        //event handler listens for change event
+        <input type="password" onChange={handleChange} />
+    );
+}
